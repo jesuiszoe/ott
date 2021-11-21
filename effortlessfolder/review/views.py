3f0e8964d0728b0496e4896dd11eed08c4fa82d4
zoe <jesuiszoekim@gmail.com>
2021-11-22 00:20:10 +0900

    수정

--- effortlessfolder/review/views.py
+++ effortlessfolder/review/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Post
 from .forms import PostForm
+
+
 
 # Create your views here.
 def post_list(request):
@@ -9,7 +11,8 @@ def post_list(request):
     return render(request, 'review/post_list.html', context)
 
 def post_detail(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
+
     context = {"post" : post}
     return render(request, 'review/post_detail.html', context)
 
