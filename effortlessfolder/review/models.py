80f3cf3804244f72ddc380e10b564e24c92565dc
zoe <jesuiszoekim@gmail.com>
2021-11-21 15:41:01 +0900

    read 구현

--- effortlessfolder/review/models.py
+++ effortlessfolder/review/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#글의 제목, 내용, 작성일, 마지막 수정일 
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True)
+    dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
+    dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now = True)
+
+    def __str__(self):
+        return self.title
+
--- effortlessfolder/review/views.py
+++ effortlessfolder/review/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    context = {"posts":posts}
+    return render(request, 'review/post_list.html', context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {"post" : post }
+    return render(request, 'review/post_detail.html', context)
