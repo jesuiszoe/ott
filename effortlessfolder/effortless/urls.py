5d9a4b8b68fa73f9feb3696085a3664e4c37337a
zoe <jesuiszoekim@gmail.com>
2021-11-23 12:36:16 +0900

    signup 구현

--- effortlessfolder/effortless/urls.py
+++ effortlessfolder/effortless/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('user.urls')),
+    path('', include('user.urls')),
     path('', include('ott.urls')),
     path('', include('review.urls')),
 ]
