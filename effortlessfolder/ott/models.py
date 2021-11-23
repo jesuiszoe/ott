14368cfbe97449cd5b8f0e99173aa3325a1ae893
zoe <jesuiszoekim@gmail.com>
2021-11-23 21:32:06 +0900

    main data

--- effortlessfolder/ott/models.py
+++ effortlessfolder/ott/models.py
@@ -2,6 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Netflix(models.Model):
-    pass
+    type = models.CharField(max_length=200)
+    title = models.CharField(max_length=50, unique =True)
+    director =  models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    release_year = models.IntegerField()
+    rating = models.CharField(max_length=200)
+    duration = models.CharField(max_length=200)
+    listed_in = models.CharField(max_length=200)
+    description= models.TextField()
+
     def __str__(self):
         return self.title
+
+class Hulu(models.Model):
+    pass
+    
+
+class Amazon(models.Model):
+    pass
+    
+
+class Disney(models.Model):
+    pass
+    
--- effortlessfolder/ott/views.py
+++ effortlessfolder/ott/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Netflix
 
 # Create your views here.
 def main(request):
     today = datetime.today().date()
-    context = {'date':today,}
+    movies = Netflix.objects.all()
+    context = {'date':today,'movies' :movies}
+    
     return render(request, 'ott/main.html', context=context)
