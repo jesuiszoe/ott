42625b656827f6936d7bc559f2bf3188acc3a34f
zoe <jesuiszoekim@gmail.com>
2021-12-04 23:35:12 +0900

    검색구현 수정

--- effortlessfolder/ott/views.py
+++ effortlessfolder/ott/views.py
@@ -50,24 +50,107 @@ def main(request):
 def amazon(request):
     today = datetime.today().date()
     movies = Amazon.objects.all()
-    context = {'date':today,'movies' :movies}
     
-    return render(request, 'ott/amazon.html', context=context)
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    
+    
+    # 조회
+    movies = Amazon.objects.order_by('id')
+    kw = request.GET.get('kw','')
+    
+
+    # 페이징처리
+    paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    
+    
+    
+    
+
+    
+    if 'kw': # 만약 검색어가 존재하면
+        movies = movies.filter(title__icontains= kw) # 해당 검색어를 포함한 queryset 가져오기
+        paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
+        page_obj = paginator.get_page(page)
+        
+        
+
+
+    context = {'date': today, 'movies': page_obj , 'page': page ,'kw':kw }
+
+    return render(request, 'ott/amazon.html', context)
+    
 
 
 def hulu(request):
     today = datetime.today().date()
     movies = Hulu.objects.all()
-    context = {'date':today,'movies' :movies}
     
-    return render(request, 'ott/hulu.html', context=context)
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    
+    
+    # 조회
+    movies = Hulu.objects.order_by('id')
+    kw = request.GET.get('kw','')
+    
+
+    # 페이징처리
+    paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    
+    
+    
+    
+
+    
+    if 'kw': # 만약 검색어가 존재하면
+        movies = movies.filter(title__icontains= kw) # 해당 검색어를 포함한 queryset 가져오기
+        paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
+        page_obj = paginator.get_page(page)
+        
+        
+
+
+    context = {'date': today, 'movies': page_obj , 'page': page ,'kw':kw }
+
+    return render(request, 'ott/hulu.html', context)
 
 def disney(request):
     today = datetime.today().date()
     movies = Disney.objects.all()
-    context = {'date':today,'movies' :movies}
     
-    return render(request, 'ott/disney.html', context=context)
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    
+    
+    # 조회
+    movies = Disney.objects.order_by('id')
+    kw = request.GET.get('kw','')
+    
+
+    # 페이징처리
+    paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    
+    
+    
+    
+
+    
+    if 'kw': # 만약 검색어가 존재하면
+        movies = movies.filter(title__icontains= kw) # 해당 검색어를 포함한 queryset 가져오기
+        paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
+        page_obj = paginator.get_page(page)
+        
+        
+
+
+    context = {'date': today, 'movies': page_obj , 'page': page ,'kw':kw }
+
+    return render(request, 'ott/disney.html', context)
+    
 
 
 def netflix(request):
