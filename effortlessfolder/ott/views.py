775f196bb134ff0bbb145313f0693f4f8336d039
zoe <jesuiszoekim@gmail.com>
2021-12-04 23:26:56 +0900

    검색 구현

--- effortlessfolder/ott/views.py
+++ effortlessfolder/ott/views.py
@@ -3,36 +3,47 @@ from django.shortcuts import render
 from datetime import datetime
 from .models import Amazon, Disney, Hulu, Netflix
 from django.db.models import Q
+from django.contrib import messages
 from django.core.paginator import Paginator
 
 
 # Create your views here.
 
 
+   
+
+
 def main(request):
     today = datetime.today().date()
     movies = Netflix.objects.all()
-    """
-    pybo 목록출력
-    """
+    
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
-    kw = request.GET.get('kw', '')  # 검색어
-
+    
+    
     # 조회
     movies = Netflix.objects.order_by('id')
+    kw = request.GET.get('kw','')
     
-    if kw:
-        movies = movies.filter(
-            Q(title__icontains=kw)   # 제목검색
-           
-        ).distinct()
 
     # 페이징처리
     paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
+    
+    
+    
+    
 
-    context = {'date': today, 'movies': page_obj , 'page': page, 'kw': kw}
+    
+    if 'kw': # 만약 검색어가 존재하면
+        movies = movies.filter(title__icontains= kw) # 해당 검색어를 포함한 queryset 가져오기
+        paginator = Paginator(movies, 10)  # 페이지당 10개씩 보여주기
+        page_obj = paginator.get_page(page)
+        
+        
+
+
+    context = {'date': today, 'movies': page_obj , 'page': page ,'kw':kw }
 
     return render(request, 'ott/main.html', context)
 
